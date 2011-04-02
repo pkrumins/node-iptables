50f523d5cf71bc8a0dd169aa94e88e64a53c72a0
Peteris Krumins <peteris.krumins@gmail.com>
2011-04-01 22:39:34 -0700

    can now delete rules. renamed deny to drop. added reject

--- index.js
+++ index.js
@@ -2,27 +2,26 @@ var spawn = require('child_process').spawn;
 
 exports.allow = function (rule) {
     rule.target = 'ACCEPT';
-    iptables(rule);
+    newRule(rule);
 }
 
-exports.deny = function (rule) {
+exports.drop = function (rule) {
     rule.target = 'DROP';
-    iptables(rule);
+    newRule(rule);
 }
+
+exports.reject = function (rule) {
+    rule.target = 'REJECT';
+    newRule(rule);
+}
+
+exports.newRule = newRule;
+exports.deleteRule = deleteRule;
 
 function iptables (rule) {
+    var args = iptablesArgs(rule);
+
     var cmd = 'iptables';
-    var args = [];
-
-    if (!rule.chain) rule.chain = 'INPUT';
-
-    if (rule.chain) args = args.concat(["-I", rule.chain]);
-    if (rule.protocol) args = args.concat(["-p", rule.protocol]);
-    if (rule.src) args = args.concat(["--src", rule.src]);
-    if (rule.dst) args = args.concat(["--dst", rule.dst]);
-    if (rule.dport) args = args.concat(["--dport", rule.dport]);
-    if (rule.sport) args = args.concat(["--sport", rule.sport]);
-
     if (rule.sudo) {
         cmd = 'sudo';
         args = ['iptables'].concat(args);
@@ -32,5 +31,30 @@ function iptables (rule) {
     proc.stderr.on('data', function (buf) {
         console.error(buf.toString());
     });
+}
+
+function iptablesArgs (rule) {
+    var args = [];
+
+    if (!rule.chain) rule.chain = 'INPUT';
+
+    if (!rule.action) rule.action = '-A';
+    if (rule.chain) args = args.concat([rule.action, rule.chain]);
+    if (rule.protocol) args = args.concat(["-p", rule.protocol]);
+    if (rule.src) args = args.concat(["--src", rule.src]);
+    if (rule.dst) args = args.concat(["--dst", rule.dst]);
+    if (rule.dport) args = args.concat(["--dport", rule.dport]);
+    if (rule.sport) args = args.concat(["--sport", rule.sport]);
+
+    return args;
+}
+
+function newRule (rule) {
+    iptables(rule);
+}
+
+function deleteRule (rule) {
+    rule.action = '-D';
+    iptables(rule);
 }
 
